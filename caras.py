05a36dc1fa2c670127736a4d966a4282b3b475b6
bertinguer <albertoil1982@gmail.com>
2023-01-16 18:55:43 +0100

    Create caras.py

---
+++ caras.py
@@ -0,0 +1,35 @@
+import boto3
+import sys
+import os
+
+# Inicializar el cliente de AWS Rekognition
+client = boto3.client("rekognition", region_name="us-east-1", aws_access_key_id="TU_KEY_DE_API_AWS", aws_secret_access_key="TU_PASS_DE_API_AWS")
+
+# Obtener la imagen origen de los parámetros
+source_image = sys.argv[1]
+
+# Definir la carpeta de imágenes a comparar
+target_folder = "caras/"
+
+# Leer la imagen origen y convertirla en formato binario
+with open(source_image, "rb") as image:
+    source_bytes = image.read()
+
+# Recorrer todas las imágenes de la carpeta de destino
+for target_image in os.listdir(target_folder):
+    # Leer la imagen de destino y convertirla en formato binario
+    with open(target_folder + target_image, "rb") as image:
+        target_bytes = image.read()
+
+    # Llamar a la API de AWS Rekognition para comparar las imágenes
+    response = client.compare_faces(
+        SourceImage={ "Bytes": source_bytes },
+        TargetImage={ "Bytes": target_bytes }
+    )
+    
+    face_matches = response.get("FaceMatches")
+    if face_matches:
+        similarity = face_matches[0]["Similarity"]
+        print("Similitud entre {} y {}: del {:.2f}%".format(source_image, target_image, similarity))
+    else:
+        print("Similitud entre {} y {}: del 0%".format(source_image, target_image))
